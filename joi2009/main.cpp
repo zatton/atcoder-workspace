1ef9fd65a48e812d2f2e258def4be45e76c085d9
zatton <ou4ei1@gmail.com>
2022-05-08 20:34:55 +0900

    joi

---
+++ joi2009/main.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define REP(i, n) for (ll i = 0; i < ll(n); i++)
+#define REPD(i, n) for (ll i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = a; i <= ll(b); i++)
+#define FORD(i, a, b) for (ll i = a; i >= ll(b); i--)
+#define FORA(i, I) for (const auto &i : I)
+#define ALL(x) x.begin(), x.end()
+#define SIZE(x) ll(x.size())
+#define INF32 2147483647
+#define INF64 9223372036854775807
+//#define MOD 1000000007
+#define F first
+#define S second
+#define debug(var)                                                             \
+  do {                                                                         \
+    cout << #var << " : ";                                                     \
+    view(var);                                                                 \
+  } while (0)
+//#define debug(var) {}
+template <typename T> void view(T e) { cout << e << endl; }
+template <typename T1, typename T2> void view(const pair<T1, T2> &v) {
+  cout << v.F << ' ' << v.S << endl;
+}
+template <typename T> void view(const vector<T> &v) {
+  for (const auto &e : v) {
+    cout << e << " ";
+  }
+  cout << endl;
+}
+template <typename T> void view(const vector<vector<T>> &vv) {
+  for (const auto &v : vv) {
+    view(v);
+  }
+}
+
+signed main() {
+  ll d, n, m;
+  cin >> d >> n >> m;
+
+  vector<ll> shop;
+  shop.push_back(0);
+  shop.push_back(d);
+  shop.push_back(-d);
+
+  REP(i, n - 1) {
+    ll s;
+    cin >> s;
+    shop.push_back(s);
+    shop.push_back(s + d);
+    shop.push_back(s - d);
+  }
+
+  sort(ALL(shop));
+
+  ll ans = 0;
+
+  REP(i, m) {
+    ll k;
+    cin >> k;
+    auto iter = lower_bound(ALL(shop), k);
+    // debug(*iter);
+    // debug(*(iter - 1));
+    ans += min(*iter - k, k - *(iter - 1));
+  }
+  cout << ans << endl;
+  return 0;
+}
